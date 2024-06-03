a42ef1bf8af480b6aa0666920b17763bd47da06a
Daffa Alif Murtaja <daffaalif1239@gmail.com>
2024-06-03 21:19:07 +0700

    fix: responsif mode done

--- resources/views/pages/dashboard/index.blade.php
+++ resources/views/pages/dashboard/index.blade.php
@@ -80,21 +80,23 @@
                 <!-- Main modal end-->
             </div>
         </div>
-        {{-- pop over start --}}
-        <div data-popover id="popover-default" role="tooltip" class="absolute z-10 invisible inline-block w-64 text-sm text-gray-500 transition-opacity duration-300 bg-white border border-golden-200 rounded-lg shadow-sm opacity-0">
-            <div class="px-3 py-2 bg-golden-100 border-b border-golden-200 rounded-t-lg">
-                <h3 class="font-semibold text-gray-900">Score File</h3>
-            </div>
-            <div class="px-3 py-2">
-                <p>merupakan hasil dari klasifikasi file dokumen oleh mesin</p>
-            </div>
-            <div data-popper-arrow></div>
-        </div>
-        {{-- pop over end --}}
 
         @include('includes.alert')
 
         <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+
+            {{-- pop over start --}}
+            <div data-popover id="popover-default" role="tooltip" class="absolute z-10 invisible inline-block w-64 text-sm text-gray-500 transition-opacity duration-300 bg-white border border-golden-200 rounded-lg shadow-sm opacity-0">
+                <div class="px-3 py-2 bg-golden-100 border-b border-golden-200 rounded-t-lg">
+                    <h3 class="font-semibold text-gray-900">Score File</h3>
+                </div>
+                <div class="px-3 py-2">
+                    <p>merupakan score hasil dari klasifikasi file dokumen oleh mesin</p>
+                </div>
+                <div data-popper-arrow></div>
+            </div>
+            {{-- pop over end --}}
+
             <table class="w-full text-sm text-left rtl:text-right text-gray-500">
                 <thead class="text-xs text-gray-700 uppercase bg-golden-300">
                     <tr>
