260db9806ed37427aef9dc170d92884faca2957a
Daffa Alif Murtaja <daffaalif1239@gmail.com>
2024-06-03 14:00:57 +0700

    feat: service classification container done

---
+++ app/Services/ClassificationServiceImpl.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Services;
+
+use App\Services\Interfaces\ClassificationService;
+use GuzzleHttp\Client;
+
+class ClassificationServiceImpl implements ClassificationService
+{
+
+    private $client;
+
+    public function __construct(Client $client)
+    {
+        $this->client = new $client;
+    }
+
+    static private function generateUrlEndpoint(string $path = null): string
+    {
+        return env("CLASSIFICATION_CONNECTION") . $path;
+    }
+
+    public function connectionGateaway()
+    {
+        return $this->client->request('GET', self::generateUrlEndpoint())->getStatusCode() == 200 ? true : false;
+    }
+
+    public function classificationDokumen(string $file_path)
+    {
+        $request = $this->client->request('POST', self::generateUrlEndpoint('classification-internal'), [
+            'query' => ['folder_file_path' => $file_path]
+        ])->getBody()->getContents();
+        
+        return json_decode($request);
+    }
+
+    public function labelCalculate(array $probabilitas, int $min = 50): ?array
+    {
+        // menampung hasil label yang lolos
+        $result = [];
+
+        foreach($probabilitas as $key => $value){
+            if ($value > $min){
+                $result[] = $key;
+            }
+        }
+
+        return $result;
+    }
+}
---
+++ app/Services/Interfaces/ClassificationService.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Services\Interfaces;
+interface ClassificationService{
+
+    /**
+     * GET Connection
+     * 
+     * mengecek koneksi terhadap endpoint
+     */
+    public function connectionGateaway();
+
+    /**
+     * Classificasi dokumen
+     * 
+     * mengklasifikasi dokumen yang diberikan
+     */
+    public function classificationDokumen(string $file_path);
+
+    /**
+     * Label Logic
+     * 
+     * menghitung hasil dari prediksi classificasi yang telah dilakukan
+     */
+    public function labelCalculate(array $probabilitas, int $min = 50): ?array;
+}
